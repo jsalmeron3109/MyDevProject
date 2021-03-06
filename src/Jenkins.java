dc544c0375f09b2ef70450fa0e187fffe9a4fb2b
Jenifer <jenifer.salmeron813@gmail.com>
2021-03-06 11:29:32 -0500

    first jenkins commit

---
+++ src/Jenkins.java
@@ -0,0 +1,9 @@
+public class Jenkins {
+    public static void main(String[] args) throws InterruptedException {
+// write your code here
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("Hello batch 8");
+            Thread.sleep(2000);
+        }
+    }
+}
